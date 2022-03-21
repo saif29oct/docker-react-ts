02244271277e82b5abfe94cc4bafcd3db6205ce1
Saiful Islam <saif29oct@gmail.com>
2022-03-22 02:17:47 +0600

    useContext Hook added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import UseStateNestedObject from "./Components/Hooks/useState";
 import TestComponent from "./Components/testComponent";
 import CustomHook from "./Components/Hooks/customHook";
+import ContextTest from "./Components/Hooks/contextHook/parent"
 
 function App() {
   return (
     // <UseStateNestedObject />
     // <TestComponent />
-    <CustomHook />
+    // <CustomHook />
+    <ContextTest />
   )
 }
 
